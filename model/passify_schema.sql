2fa4d79f6ca0c87c0c3e65edb56690be650a99d7
ReverbDesigns <jordymcbell@gmail.com>
2020-11-24 13:16:55 -0800

    go

---
+++ model/passify_schema.sql
@@ -0,0 +1,20 @@
+USE passify_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE logins (
+	id INT AUTO_INCREMENT NOT NULL,
+    website VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    passwordStrength INT NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    userId INT references users(id),
+    PRIMARY KEY (id)
+)
+
